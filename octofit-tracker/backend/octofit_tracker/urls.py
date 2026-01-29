67c03c312f3bf9252912b75daef009a1355dfb98
kishorebalu21 <Kishore.Balu@gds.ey.com>
2026-01-29 13:05:32 +0000

    Modifed py files

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -30,6 +30,7 @@ router.register(r'leaderboard', LeaderboardViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
     path('api/', api_root, name='api-root'),
     path('api/', include(router.urls)),
 ]
